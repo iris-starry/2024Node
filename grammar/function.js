3ac97ce773f513dddfb61448c7e7d906acb16f20
iris-starry <iris_starry_@naver.com>
2024-03-14 16:09:57 +0900

    익명함수 - 기본

--- grammar/function.js
+++ grammar/function.js
@@ -1,14 +1,20 @@
-function 문자출력함수(문자) {
-    console.log('문자출력함수 ' + 문자)
-}
-문자출력함수('a')
+// function 문자출력함수(문자) {
+//     console.log('문자출력함수 ' + 문자)
+// }
+// 문자출력함수('a')
 
-// 함수명을 변수처럼 사용
-printChar = 문자출력함수
-printChar('b')
+// // 함수명을 변수처럼 사용
+// printChar = 문자출력함수
+// printChar('b')
 
 // 익명함수 : 함수의 이름이 없는 함수
-익명함수 = function(문자) {
+// 익명함수 = function(문자) {
+//     console.log('익명함수 ' + 문자)
+// }
+// 익명함수('c')
+
+// arrow function : 익명함수 코드를 간소화
+익명함수 = (문자) => {
     console.log('익명함수 ' + 문자)
 }
 익명함수('c')
