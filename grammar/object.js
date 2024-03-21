f10b3cf061b085909909729c12be10fb39c931b5
iris-starry <iris_starry_@naver.com>
2024-03-21 14:32:01 +0900

    object 속성 제거

--- grammar/object.js
+++ grammar/object.js
@@ -1,10 +1,13 @@
 const cathy = {
     name : "cathy",
-    // 속성 이름은 따옴표 처리 안해도 된다.
-    age : 19,
+    // 속성 이름은 따옴표 처리 안해도 된다
+    "age" : 19,
     skills : ["자바스크립트", "파이썬", "코볼"]
 }
 
 console.log(cathy)
 console.log(cathy.name)
 console.log(cathy["name"])
+
+delete cathy.skills
+console.log(cathy)
